2f6b98587aa159f7ad93d8d9a087fe7f34b46d2d
Minh Phien <minhphien012@gmal.com>
2025-03-26 16:01:30 +0700

    Revert "xóa form liên hệ"
    This reverts commit d5cbf224f3aa2b03b3b1a22a34f9750d4abdcf88.

--- ContactList/ContactList/MainForm.Designer.cs
+++ ContactList/ContactList/MainForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-        
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,13 +40,23 @@
             this.label1.Size = new System.Drawing.Size(113, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Danh sách liên hệ";
-            
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Thêm liên hệ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-           
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "MainForm";
             this.Text = "Danh sách liên hệ";
@@ -57,6 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-   
+        private System.Windows.Forms.Button button1;
     }
 }
